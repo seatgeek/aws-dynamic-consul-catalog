6b942d066e3e031589fb1306a2965a515ed4c1de
Artur Santos <artur.santos@talkdesk.com>
2024-10-13 02:26:42 +0100

    fix rds maser/replica tags

--- service/rds/instances.go
+++ service/rds/instances.go
@@ -32,8 +32,8 @@ func (r *RDS) writeBackendCatalogInstances(instance *config.RDSInstances, logger
 	addr := aws.ToString(instance.RDSInstance.Endpoint.Address)
 	port := aws.ToInt64(aws.Int64(int64(*instance.RDSInstance.Endpoint.Port)))
 
-	isSlave := instance.RDSInstance.ReadReplicaSourceDBInstanceIdentifier != nil
-	isMaster := len(instance.RDSInstance.ReadReplicaDBInstanceIdentifiers) > 0
+	isMaster := instance.RDSInstance.ReadReplicaDBInstanceIdentifiers != nil
+	isSlave := len(instance.RDSInstance.ReadReplicaDBInstanceIdentifiers) > 0
 
 	logger.Debugf("  ID:   %s", id)
 	logger.Debugf("  Name: %s", name)
